c1238a058df643ce40e0fb2570e9460c85203dfa
Mario <71526010+petronijevicm@users.noreply.github.com>
2024-09-25 13:37:40 +0200

    Add setup script for NextDNS autoconnect

---
+++ Setup.sh
@@ -0,0 +1,22 @@
+# This Part of the script calls NextDNS server to update Linked-ip whenever network(VPN) reconnects
+#!/bin/bash
+DISPATCHER_SCRIPT="/etc/NetworkManager/dispatcher.d/99-nextdns-link"
+
+sudo bash -c "cat > $DISPATCHER_SCRIPT" << 'EOF'
+#!/bin/bash
+
+# Check if the connection is up
+if [ "$2" = "up" ]; then
+    sleep 2  # Wait for 5 seconds
+    curl -s https://link-ip.nextdns.io/[put your config here] >> /var/log/my-network-command.log 2>&1
+fi
+EOF
+
+sudo chmod u+x "$DISPATCHER_SCRIPT"
+
+# Create the log file and set permissions
+sudo touch /var/log/my-network-command.log
+sudo chmod 644 /var/log/my-network-command.log
+
+echo "NetworkManager NextDNS script has been set up successfully."
+
